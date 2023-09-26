c384c76cb5cb28153a8fffe30b44eafd656601f3
Gabriel Fairbairn <97549201+gabrieljmf@users.noreply.github.com>
2023-09-26 12:27:56 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const math = require("./modules/math");
+const url = require("url");
+const serverTime = require("./modules/time");
+
+http
+  .createServer(function (req, res) {
+    let name = "";
+    const fullUrl = url.parse(req.url, true);
+    if (fullUrl.query.name != undefined) {
+      name = fullUrl.query.name;
+    } else {
+      name = "Gabriel";
+    }
+
+    const data = `Welcome <b>${name}</b><br />`;
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(data);
+
+    res.end(`<button ><a href='/?name=gabe'>Send Name</a></button>`);
+  })
+  .listen(8888);
+
+console.log("server is running and listening ...");
---
+++ modules/math.js
@@ -0,0 +1,16 @@
+exports.add = (x, y) => {
+  return x + y;
+};
+
+exports.subtract = (x, y) => {
+  return x - y;
+};
+
+const PI = 3.1415926535;
+exports.area = (r) => {
+  return PI * r * 2;
+};
+// notes from lecture
+// exports.area = function (r) {
+//   return r * PI * 2;
+// };
